df0425715fb082bccddd02f7fdfb9588029f8ad1
cüneyt yazici <cuuneyt_yazicii@hotmail.com>
2020-06-29 00:06:39 +0300

    28062020 seeder

---
+++ webproje/database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = bcrypt('123');
+
+        $users = [
+            ['name' => 'uye', 'password' => $password, 'email' => 'uye@uye','id'=>'3','email_verified_at'=>'','yetki'=>'2','remembertoken'=>'','created_at'=>'2020-06-28 12:29:13','updated_at'=>'2020-06-28 12:29:13'],
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
