2297a2ea49fa46a6d92cdb2bb2681ba4e2b01b35
cüneyt yazici <cuuneyt_yazicii@hotmail.com>
2020-06-29 12:45:07 +0300

    2906202012/44

--- webproje/config/database.php
+++ webproje/config/database.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url('postgres://eukcjrzeskwrdr:909fbfb98a49e5aaf728290a9dd9cb8c02a5fa7cc0be3e8ac54004fdaa8c87fe@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d43
+scevfjrgph
+');
 return [
 
     /*
@@ -65,17 +67,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
